74b78788e910fe54a01f38b53880a7ff55cd678c
jrodolforeis <joserodolfomendes@hotmail.com>
2022-06-04 01:16:33 -0300

    It's WORKING!

--- src/main/java/buffer/Buffer.java
+++ src/main/java/buffer/Buffer.java
@@ -8,20 +8,45 @@ public class Buffer {
     private List<Integer> buffer = new ArrayList<>();
     private Random r = new Random();
 
-    public synchronized int produce() throws InterruptedException {
-        while(this.buffer.size() == 5){
-            wait();
+    public void produce() throws InterruptedException {
+        while(true) {
+            while (this.buffer.size() == 5) {
+                synchronized (this) {
+                    wait();
+                }
+            }
+            synchronized (this) {
+                int valor = Math.abs(this.r.nextInt());
+                this.buffer.add(valor);
+                System.out.println("O valor " + valor + " foi produzido e inserido no buffer.");
+                notifyAll();
+            }
+            Thread.sleep((r.nextInt(5)+1)*1000);
         }
-        int valor = Math.abs(this.r.nextInt());
-        this.buffer.add(valor);
-        return valor;
     }
 
-    public synchronized int consume() throws InterruptedException {
-        while(this.buffer.size() == 0){
-            wait();
+    public void consume() throws InterruptedException {
+        while(true) {
+            while (this.buffer.size() == 0) {
+                synchronized (this) {
+                    wait();
+                }
+            }
+            synchronized (this) {
+                int valor = this.buffer.remove(0);
+                System.out.println("O valor " + valor + " foi consumido e removido do buffer.");
+                notifyAll();
+            }
+            Thread.sleep((r.nextInt(5)+1)*1000);
         }
-        int valor = this.buffer.remove(0);
-        return valor;
+    }
+
+    public List<Integer> bufferState() throws InterruptedException {
+        while(true) {
+            synchronized (this) {
+                System.out.println(this.buffer.size() + " elementos: " + this.buffer);
+            }
+            Thread.sleep((r.nextInt(5) + 1) * 1000);
+        }
     }
 }
--- src/main/java/controller/Main.java
+++ src/main/java/controller/Main.java
@@ -1,7 +1,39 @@
 package controller;
 
-public class Main {
-    public static void main(String[] args) {
+import buffer.Buffer;
 
+public class Main {
+
+    public static void main(String[] args){
+        final Buffer buffer = new Buffer();
+
+        Thread producer = new Thread(() -> {
+            try {
+                buffer.produce();
+            } catch (InterruptedException e) {
+                e.printStackTrace();
+            }
+        });
+
+        Thread consumer = new Thread(() -> {
+            try {
+                buffer.consume();
+            } catch (InterruptedException e) {
+                e.printStackTrace();
+            }
+        });
+
+        Thread bufferState = new Thread(() -> {
+            try {
+                buffer.bufferState();
+            } catch (InterruptedException e) {
+                e.printStackTrace();
+            }
+        });
+
+        producer.start();
+        consumer.start();
+        bufferState.start();
     }
+
 }
